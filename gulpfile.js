c56f334a6e8826e443510946b3c63dcb009dc29e
alx2das <alx2das@gmail.com>
2017-02-01 18:47:55 +0500

    update gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,135 @@
 'use strict';
 
-var gulp = require('gulp'),
-    del = require('del'),
+var pkg = require('./package.json'),
+    gulp = require('gulp'),
+    autoprefixer = require('autoprefixer'),
+    postcss = require('gulp-postcss'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     minifyCSS = require('gulp-minify-css'),
-    gulpIf = require('gulp-if'),
+    rename = require("gulp-rename"),
+    del = require('del'),
     runSequence = require('run-sequence');
 
-
+// очистка директории
 gulp.task('clean', function () {
-    return del('dist')
+    return del('dist');
 });
 
-gulp.task('copy', function () {
+// обьединение файлов
+gulp.task('concat', function (callback) {
+    runSequence(
+        ['concat:js', 'concat:css'],
+        callback
+    );
+});
+
+// обьединение JS файлов
+gulp.task('concat:js', function () {
     return gulp
-        .src('src/**/*.*')
-        .pipe(gulpIf('*.css', concat('battlefield.css')))
-        .pipe(gulpIf('*.js', concat('battlefield.js')))
-        .pipe(gulp.dest('dist'))
+        .src([
+            'src/js/battlefield.options.js',
+            'src/js/battlefield.class-battlefield.js',
+            'src/js/battlefield.class-field.js',
+            'src/js/battlefield.class-battle.js',
+            'src/js/battlefield.class-gameui.js',
+            'src/js/battlefield.helper.js'
+        ])
+        .on('data', function (file) {
+            file.contents = new Buffer(file.contents.toString() + '\n');
+        })
+        .pipe(concat('battlefield.js'))
+        .on('data', function (file) {
+            file.contents = new Buffer(
+                description() +
+                '(function(window){' +
+                '   \n' +
+                '   "use strict";' +
+                '   \n\n' +
+                file.contents.toString() +
+                '   \n' +
+                '   window.Battlefield = Battlefield;' +
+                '   \n' +
+                '})(window);'
+            );
+        })
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('copy:min', function () {
+// обьединение CSS файлов
+gulp.task('concat:css', function () {
     return gulp
-        .src('src/**/*.*')
-        .pipe(gulpIf('*.css', concat('battlefield.min.css'))).pipe(gulpIf('*.css', minifyCSS()))
-        .pipe(gulpIf('*.js', concat('battlefield.min.js'))) .pipe(gulpIf('*.js', uglify()))
-        .pipe(gulp.dest('dist'))
+        .src('src/css/*.css')
+        .pipe(postcss([autoprefixer()]))
+        .pipe(concat('battlefield.css'))
+        .on('data', function (file) {
+                file.contents = new Buffer(
+                    description() +
+                    file.contents.toString()
+                )
+        })
+        .pipe(gulp.dest('dist'));
 });
 
+// минификация файлов
+gulp.task('min', function (callback) {
+    runSequence(
+        ['min:js', 'min:css'],
+        callback
+    );
+});
 
-gulp.task('build', function (callback) {
+// минификация JS файлов
+gulp.task('min:js', function () {
+    return gulp
+        .src(['dist/battlefield.js'])
+        .pipe(rename('battlefield.min.js'))
+        .pipe(uglify())
+        .on('data', function (file) {
+            file.contents = new Buffer(
+                description() +
+                file.contents.toString()
+            );
+        })
+        .pipe(gulp.dest('dist'));
+});
+
+// минификация CSS файлов
+gulp.task('min:css', function () {
+    return gulp
+        .src(['dist/battlefield.css'])
+        .pipe(rename('battlefield.min.css'))
+        .pipe(minifyCSS())
+        .on('data', function (file) {
+            file.contents = new Buffer(
+                description() +
+                file.contents.toString()
+            )
+        })
+        .pipe(gulp.dest('dist'));
+});
+
+// главная задача
+gulp.task('default', function (callback) {
     runSequence(
         'clean',
-        ['copy', 'copy:min'],
+        'concat',
+        'min',
         callback
-    )
+    );
 });
+
+// описание для файлов
+function description() {
+    return '' +
+        '/**\n' +
+        ' * ' + pkg.name + ' v' + pkg.version + '\n' +
+        ' * ' + pkg.description + '\n' +
+        ' * \n' +
+        ' * repository: ' + pkg.repository.url + '\n' +
+        ' * bugs: ' + pkg.repository + '\n' +
+        ' * \n' +
+        ' * Copyright 2017, ' + pkg.author + '\n' +
+        ' * Date: ' + new Date().toDateString() + '\n' +
+        ' */\n';
+}
--- src/js/battlefield.helper.js
+++ src/js/battlefield.helper.js
@@ -1,209 +1,209 @@
-var h = {
-    playerName: {
-        def: {
-            kUser: 'Игрок',
-            kBrain: 'Компьютер'
-        }
-    },
-
-    rand: function (min, max) {
-        min = min || 0;
-        max = max || 100;
-
-        return parseInt(Math.random() * (max - min + 1) + min);
-    },
-    getLetter: function (key, operand) {
-        var operand = operand || '',
-            alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        if (key > alphabet.length)
-            return h.getLetter(key - alphabet.length, (operand == '' ? 1 : operand + 1));
-
-        return alphabet[key] + operand;
-    },
-    shuffle: function (arr) {
-        var inx, buffer;
-        for (var i = 0; i < arr.length - 1; i++) {
-            inx = this.rand(0, arr.length - 1);
-            buffer = arr[inx];
-
-            arr[inx] = arr[arr.length - 1];
-            arr[arr.length - 1] = buffer;
-        }
-        return arr;
-    },
-    getMessage: function (type) {
-        var mess = {
-            start_game: 'Начать игру',
-            new_game: 'Новая игра',
-            game_over: 'Игра закончена',
-            you_lose: 'Ты проиграл',
-            you_winner: 'Ты выиграл',
-            options: 'Настройки',
-            update_options: 'Изменить настройки',
-            close: 'Закрыть',
-            update_page: 'Обновить страницу',
-            set_default_params: 'Настройки по умолчанию',
-            save: 'Сохранить',
-            filter: 'Фильтр',
-            full: 'Все',
-
-            log_kill: 'ранил',
-            log_death: 'убил',
-            log_past: 'мимо',
-
-            // error
-            err_invalid_selector: 'Указанный HTML блок на странице не найден',
-            err_max_iteration: 'Не получилось установить корабли на игровое поле, измените настройки',
-            err_invalid_player: 'Неизвестный игрок',
-            err_invalid_field: 'Некорректное игровое поле',
-            err_player_name: 'Переданны не корректные ключи играков',
-            err_size_field: 'Игровое поле не должно быть меньше 10х10 или больше 25х25, измените настройки',
-            err_barrier: 'Не корректный список кораблей',
-            err_invalid_level: 'Указан несуществующий уровень сложности',
-
-            // info
-            info_log_title: 'Состояние игрового поля',
-            info_title_error: 'Критическая ошибка',
-            info_title_range_error: 'Некорректные настройки',
-            info_winner: 'Замечательная победа,<br>попробуешь еще?',
-            info_loser: 'В следующий раз повезет больше,<br>попробуешь еще?'
-        };
-
-        return typeof mess[type] == 'string' && mess[type].length > 0 ? mess[type] : type;
-    },
-    getPlayerName: function (fKey) {
-        var key = fKey == options.player.kUser ? 'kUser' : 'kBrain';
-        return this.playerName.def[key];
-    },
-    showExceptions: function (err) {
-        var title = '',
-            content = '',
-            button = [];
-
-        switch (err.name) {
-            case ('Error'):
-                title = h.getMessage('info_title_error');
-                content = err.message;
-                button = [{
-                    elValue: h.getMessage('set_default_params'),
-                    onClick: function (modal) {
-                        modal.close();
-                        var b = new Battlefield(options.htmlSelector, options);
-                        b.setLevel('middle').run();
-                    }
-                }];
-                break;
-            case ('RangeError'):
-                title = h.getMessage('info_title_range_error');
-                content = err.message;
-                button = [{
-                    elValue: h.getMessage('set_default_params'),
-                    onClick: function (modal) {
-                        modal.close();
-                        var b = new Battlefield(options.htmlSelector, options);
-                        b.setLevel('middle').run();
-                    }
-                }];
-                break;
-            default:
-                title = err.name;
-                content = err.message;
-        }
-
-        this.modalWindow(title, content, button);
-    },
-    modalWindow: function (title, contentHTML, button) {
-        var modal = {
-            font: false,
-            window: false,
-
-            construct: function () {
-                this.createHTML().autoPosition();
-
-                if (!(button instanceof Array) || button.length == 0) {
-                    this.window.querySelector('.footer').style.display = 'none';
-                    return false;
-                }
-
-                var footer = this.window.querySelector('.footer'),
-                    on = [];
-
-                for (var i = 0; i < button.length; i++) {
-                    var btn = button[i],
-                        dopClass = typeof button[i].elClass == 'string' ? ' ' + btn.elClass : '';
-
-                    on[i] = document.createElement('button');
-                    on[i].setAttribute('class', 'btn' + dopClass);
-                    on[i].setAttribute('id', 'btn_' + i);
-                    on[i].innerHTML = btn.elValue;
-                    on[i].addEventListener('click', eventListener(i, button), false);
-
-                    footer.appendChild(on[i]);
-                }
-            },
-            createHTML: function () {
-                var _font = document.querySelector('.bf-modal-font'),
-                    _window = document.querySelector('.bf-modal-window');
-                if (_font != null) document.body.removeChild(_font);
-                if (_window != null) document.body.removeChild(_window);
-
-
-                this.font = document.createElement('div');
-                this.font.setAttribute('class', 'bf-modal-font');
-
-                this.window = document.createElement('div');
-                this.window.setAttribute('class', 'bf-modal-window');
-                this.window.innerHTML =
-                    '<div class="content">' +
-                    '   <div class="header">' + title + '</div>' +
-                    '   <div class="cont">' + contentHTML + '</div>' +
-                    '   <div class="footer"></div>' +
-                    '</div>';
-
-                document.body.appendChild(this.font);
-                document.body.appendChild(this.window);
-
-                return this;
-            },
-            autoPosition: function () {
-                var wid2 = this.window.clientWidth / 2,
-                    hei2 = this.window.clientHeight / 2;
-
-                this.window.style.marginTop = hei2 * (-1) + 'px';
-                this.window.style.marginLeft = wid2 * (-1) + 'px';
-            },
-            close: function () {
-                document.body.removeChild(this.font);
-                document.body.removeChild(this.window);
+    var h = {
+        playerName: {
+            def: {
+                kUser: 'Игрок',
+                kBrain: 'Компьютер'
             }
-        };
+        },
 
-        modal.construct();
+        rand: function (min, max) {
+            min = min || 0;
+            max = max || 100;
 
+            return parseInt(Math.random() * (max - min + 1) + min);
+        },
+        getLetter: function (key, operand) {
+            var operand = operand || '',
+                alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            if (key > alphabet.length)
+                return h.getLetter(key - alphabet.length, (operand == '' ? 1 : operand + 1));
 
-        // private method....................
-        // ..................................
+            return alphabet[key] + operand;
+        },
+        shuffle: function (arr) {
+            var inx, buffer;
+            for (var i = 0; i < arr.length - 1; i++) {
+                inx = this.rand(0, arr.length - 1);
+                buffer = arr[inx];
 
-        // вешает событие на кнопку в модальном окне
-        function eventListener(i, obj) {
-            return function (event) {
-                if (typeof obj[i].onClick == 'function')
-                    obj[i].onClick(modal, event);
+                arr[inx] = arr[arr.length - 1];
+                arr[arr.length - 1] = buffer;
+            }
+            return arr;
+        },
+        getMessage: function (type) {
+            var mess = {
+                start_game: 'Начать игру',
+                new_game: 'Новая игра',
+                game_over: 'Игра закончена',
+                you_lose: 'Ты проиграл',
+                you_winner: 'Ты выиграл',
+                options: 'Настройки',
+                update_options: 'Изменить настройки',
+                close: 'Закрыть',
+                update_page: 'Обновить страницу',
+                set_default_params: 'Настройки по умолчанию',
+                save: 'Сохранить',
+                filter: 'Фильтр',
+                full: 'Все',
+
+                log_kill: 'ранил',
+                log_death: 'убил',
+                log_past: 'мимо',
+
+                // error
+                err_invalid_selector: 'Указанный HTML блок на странице не найден',
+                err_max_iteration: 'Не получилось установить корабли на игровое поле, измените настройки',
+                err_invalid_player: 'Неизвестный игрок',
+                err_invalid_field: 'Некорректное игровое поле',
+                err_player_name: 'Переданны не корректные ключи играков',
+                err_size_field: 'Игровое поле не должно быть меньше 10х10 или больше 25х25, измените настройки',
+                err_barrier: 'Не корректный список кораблей',
+                err_invalid_level: 'Указан несуществующий уровень сложности',
+
+                // info
+                info_log_title: 'Состояние игрового поля',
+                info_title_error: 'Критическая ошибка',
+                info_title_range_error: 'Некорректные настройки',
+                info_winner: 'Замечательная победа,<br>попробуешь еще?',
+                info_loser: 'В следующий раз повезет больше,<br>попробуешь еще?'
             };
-        }
-    },
-    animateOpacity: function (element, time) {
-        element.style.opacity = 1;
 
-        var t = setInterval(function () {
-            element.style.opacity = element.style.opacity - (100 / (time / 0.1));
-            if (element.style.opacity <= 0) {
-                clearInterval(t);
-                try {
-                    element.parentNode.removeChild(element);
-                } catch (e) {
-                }
+            return typeof mess[type] == 'string' && mess[type].length > 0 ? mess[type] : type;
+        },
+        getPlayerName: function (fKey) {
+            var key = fKey == options.player.kUser ? 'kUser' : 'kBrain';
+            return this.playerName.def[key];
+        },
+        showExceptions: function (err) {
+            var title = '',
+                content = '',
+                button = [];
+
+            switch (err.name) {
+                case ('Error'):
+                    title = h.getMessage('info_title_error');
+                    content = err.message;
+                    button = [{
+                        elValue: h.getMessage('set_default_params'),
+                        onClick: function (modal) {
+                            modal.close();
+                            var b = new Battlefield(options.htmlSelector, options);
+                            b.setLevel('middle').run();
+                        }
+                    }];
+                    break;
+                case ('RangeError'):
+                    title = h.getMessage('info_title_range_error');
+                    content = err.message;
+                    button = [{
+                        elValue: h.getMessage('set_default_params'),
+                        onClick: function (modal) {
+                            modal.close();
+                            var b = new Battlefield(options.htmlSelector, options);
+                            b.setLevel('middle').run();
+                        }
+                    }];
+                    break;
+                default:
+                    title = err.name;
+                    content = err.message;
             }
-        }, 1);
-    }
-};
+
+            this.modalWindow(title, content, button);
+        },
+        modalWindow: function (title, contentHTML, button) {
+            var modal = {
+                font: false,
+                window: false,
+
+                construct: function () {
+                    this.createHTML().autoPosition();
+
+                    if (!(button instanceof Array) || button.length == 0) {
+                        this.window.querySelector('.footer').style.display = 'none';
+                        return false;
+                    }
+
+                    var footer = this.window.querySelector('.footer'),
+                        on = [];
+
+                    for (var i = 0; i < button.length; i++) {
+                        var btn = button[i],
+                            dopClass = typeof button[i].elClass == 'string' ? ' ' + btn.elClass : '';
+
+                        on[i] = document.createElement('button');
+                        on[i].setAttribute('class', 'btn' + dopClass);
+                        on[i].setAttribute('id', 'btn_' + i);
+                        on[i].innerHTML = btn.elValue;
+                        on[i].addEventListener('click', eventListener(i, button), false);
+
+                        footer.appendChild(on[i]);
+                    }
+                },
+                createHTML: function () {
+                    var _font = document.querySelector('.bf-modal-font'),
+                        _window = document.querySelector('.bf-modal-window');
+                    if (_font != null) document.body.removeChild(_font);
+                    if (_window != null) document.body.removeChild(_window);
+
+
+                    this.font = document.createElement('div');
+                    this.font.setAttribute('class', 'bf-modal-font');
+
+                    this.window = document.createElement('div');
+                    this.window.setAttribute('class', 'bf-modal-window');
+                    this.window.innerHTML =
+                        '<div class="content">' +
+                        '   <div class="header">' + title + '</div>' +
+                        '   <div class="cont">' + contentHTML + '</div>' +
+                        '   <div class="footer"></div>' +
+                        '</div>';
+
+                    document.body.appendChild(this.font);
+                    document.body.appendChild(this.window);
+
+                    return this;
+                },
+                autoPosition: function () {
+                    var wid2 = this.window.clientWidth / 2,
+                        hei2 = this.window.clientHeight / 2;
+
+                    this.window.style.marginTop = hei2 * (-1) + 'px';
+                    this.window.style.marginLeft = wid2 * (-1) + 'px';
+                },
+                close: function () {
+                    document.body.removeChild(this.font);
+                    document.body.removeChild(this.window);
+                }
+            };
+
+            modal.construct();
+
+
+            // private method....................
+            // ..................................
+
+            // вешает событие на кнопку в модальном окне
+            function eventListener(i, obj) {
+                return function (event) {
+                    if (typeof obj[i].onClick == 'function')
+                        obj[i].onClick(modal, event);
+                };
+            }
+        },
+        animateOpacity: function (element, time) {
+            element.style.opacity = 1;
+
+            var t = setInterval(function () {
+                element.style.opacity = element.style.opacity - (100 / (time / 0.1));
+                if (element.style.opacity <= 0) {
+                    clearInterval(t);
+                    try {
+                        element.parentNode.removeChild(element);
+                    } catch (e) {
+                    }
+                }
+            }, 1);
+        }
+    };
